0e33c030da6b8ad6b1cc6d4ef5fa4853690e19c5
Sergey.Butchak <Sergei.Butchak@gmail.com>
2021-07-13 15:26:22 +0400

    added admin-user-create

--- admins/views.py
+++ admins/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import messages
+from django.urls import reverse
+from users.models import User
+from admins.forms import UserAdminRegistrationForm
 
 def admins(request):
     context = {'title': 'GeekShop - Admin'}
@@ -7,12 +10,26 @@ def admins(request):
 
 
 def admin_users_read(request):
-    context = {'title': 'GeekShop - Admin'}
+    context = {
+        'title': 'GeekShop - Admin',
+        'users': User.objects.all(),
+    }
     return render(request, 'admins/admin-users-read.html', context)
 
 
 def admin_users_create(request):
-    context = {'title': 'GeekShop - Admin'}
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Пользователь создан')
+            return HttpResponseRedirect(reverse('admins:create'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {
+        'title': 'Admin - Создание пользователя.',
+        'form': form
+    }
     return render(request, 'admins/admin-users-create.html', context)
 
 
