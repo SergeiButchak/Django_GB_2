339ade6beba151c04b42666b28525b1887d41441
Sergey.Butchak <Sergei.Butchak@gmail.com>
2021-07-14 10:58:17 +0400

    added categories-read in admin

--- admins/urls.py
+++ admins/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from admins.views import admins, admin_users_read, admin_users_create, admin_users_update, admin_users_delete
+from admins.views import admin_cat_read, admin_cat_create, admin_cat_update, admin_cat_delete
 
 app_name = 'admins'
 
@@ -9,4 +10,9 @@ urlpatterns = [
     path('users/create/', admin_users_create, name='users_create'),
     path('users/update/<int:pk>/', admin_users_update, name='users_update'),
     path('users/delete/<int:pk>/', admin_users_delete, name='users_delete'),
+    path('cat/read/', admin_cat_read, name='cat_read'),
+    path('cat/create/', admin_cat_create, name='cat_create'),
+    path('cat/update/<int:pk>/', admin_cat_update, name='cat_update'),
+    path('cat/delete/<int:pk>/', admin_cat_delete, name='cat_delete'),
+
 ]
--- admins/views.py
+++ admins/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
 from django.urls import reverse
 from users.models import User
+from products.models import ProductCategory, Product
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
@@ -60,6 +61,60 @@ def admin_users_update(request, pk):
 
 @user_passes_test(lambda u: u.is_staff)
 def admin_users_delete(request, pk):
+    user = User.objects.get(id=pk)
+    user.is_active = False
+    user.save()
+    return HttpResponseRedirect(reverse('admins:users_read'))
+
+
+@user_passes_test(lambda u: u.is_staff)
+def admin_cat_read(request):
+    context = {
+        'title': 'GeekShop - Admin',
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'admins/admin-cat-read.html', context)
+
+
+@user_passes_test(lambda u: u.is_staff)
+def admin_cat_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Пользователь создан')
+            return HttpResponseRedirect(reverse('admins:read'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {
+        'title': 'Admin - Создание пользователя.',
+        'form': form
+    }
+    return render(request, 'admins/admin-users-create.html', context)
+
+
+@user_passes_test(lambda u: u.is_staff)
+def admin_cat_update(request, pk):
+    user = User.objects.get(id=pk)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Измененеия сохранены.')
+            return HttpResponseRedirect(reverse('admins:users_read'))
+    else:
+        form = UserAdminProfileForm(instance=user)
+        # print(r.total_quantity)
+    context = {
+        'title': 'Admin - Изменение пользователя.',
+        'form': form,
+        'user': user
+    }
+    return render(request, 'admins/admin-users-update-delete.html', context)
+
+
+@user_passes_test(lambda u: u.is_staff)
+def admin_cat_delete(request, pk):
     user = User.objects.get(id=pk)
     user.is_active = False
     user.save()
