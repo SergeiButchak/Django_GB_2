ac5e664389e7ddafb3fe979549892df6f0a4565f
Sergey.Butchak <Sergei.Butchak@gmail.com>
2021-07-20 21:06:48 +0400

    added CBV for products\views

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from products.views import products
+from products.views import ProductListView
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index'),
-    path('category/<int:category_id>/', products, name='categories'),
-    path('page/<int:page>/', products, name='page'),
-    path('category/<int:category_id>/page/<int:page>/', products, name='cat_page'),
+    path('', ProductListView.as_view(), name='index'),
+    path('category/<int:category_id>/', ProductListView.as_view(), name='categories'),
+    path('page/<int:page>/', ProductListView.as_view(), name='page'),
+    path('category/<int:category_id>/page/<int:page>/', ProductListView.as_view(), name='cat_page'),
+    # path('category/<int:category_id>/', products, name='categories'),
+    # path('page/<int:page>/', products, name='page'),
+    # path('category/<int:category_id>/page/<int:page>/', products, name='cat_page'),
 ]
